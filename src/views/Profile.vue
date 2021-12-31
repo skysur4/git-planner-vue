452eee3cde42e46c23df325dfaab7c203454123b
김창환(클라우드플랫폼그룹) <chris.ch.kim@partner.samsung.com>
2021-12-31 14:17:13 +0900

    프로필 개발 중

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -12,35 +12,25 @@
               <div class="profile">
                 <div class="avatar">
                   <img
-                    :src="img"
+                    :src="userInfo.avatar_url"
                     alt="Circle Image"
                     class="img-raised rounded-circle img-fluid"
                   />
                 </div>
                 <div class="name">
-                  <h3 class="title">Carla Hortensia</h3>
-                  <h6>Designer</h6>
+                  <h3 class="title">{{userInfo.name}}</h3>
+                  <h6>{{userInfo.login}}</h6>
                   <md-button
                     href="javascript:void(0)"
                     class="md-just-icon md-simple md-dribbble"
-                    ><i class="fab fa-dribbble"></i
-                  ></md-button>
-                  <md-button
-                    href="javascript:void(0)"
-                    class="md-just-icon md-simple md-twitter"
-                    ><i class="fab fa-twitter"></i
-                  ></md-button>
-                  <md-button
-                    href="javascript:void(0)"
-                    class="md-just-icon md-simple md-pinterest"
-                    ><i class="fab fa-pinterest"></i
+                    ><i class="fab fa-github"></i
                   ></md-button>
                 </div>
               </div>
             </div>
           </div>
           <div class="description text-center">
-            <p>
+            <p>{{userInfo.bio}}
               An artist of considerable range, Chet Faker — the name taken by
               Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
               and records all of his own music, giving it a warm, intimate feel
@@ -104,52 +94,85 @@
 
 <script>
 import { Tabs } from "@/components";
+
+import github from "@/var/github";
+import authUtils from "@/plugins/authentications";
+
 export default {
-  components: {
-    Tabs
-  },
-  bodyClass: "profile-page",
-  data() {
-    return {
-      tabPane1: [
-        { image: require("@/assets/img/examples/studio-1.jpg") },
-        { image: require("@/assets/img/examples/studio-2.jpg") },
-        { image: require("@/assets/img/examples/studio-4.jpg") },
-        { image: require("@/assets/img/examples/studio-5.jpg") }
-      ],
-      tabPane2: [
-        { image: require("@/assets/img/examples/olu-eletu.jpg") },
-        { image: require("@/assets/img/examples/clem-onojeghuo.jpg") },
-        { image: require("@/assets/img/examples/cynthia-del-rio.jpg") },
-        { image: require("@/assets/img/examples/mariya-georgieva.jpg") },
-        { image: require("@/assets/img/examples/clem-onojegaw.jpg") }
-      ],
-      tabPane3: [
-        { image: require("@/assets/img/examples/mariya-georgieva.jpg") },
-        { image: require("@/assets/img/examples/studio-3.jpg") },
-        { image: require("@/assets/img/examples/clem-onojeghuo.jpg") },
-        { image: require("@/assets/img/examples/olu-eletu.jpg") },
-        { image: require("@/assets/img/examples/studio-1.jpg") }
-      ]
-    };
-  },
-  props: {
-    header: {
-      type: String,
-      default: require("@/assets/img/city-profile.jpg")
-    },
-    img: {
-      type: String,
-      default: require("@/assets/img/faces/christian.jpg")
-    }
-  },
-  computed: {
-    headerStyle() {
-      return {
-        backgroundImage: `url(${this.header})`
-      };
-    }
-  }
+	components: {
+	  Tabs
+	},
+	bodyClass: "profile-page",
+	data() {
+	  return {
+	    tabPane1: [
+	      { image: require("@/assets/img/examples/studio-1.jpg") },
+	      { image: require("@/assets/img/examples/studio-2.jpg") },
+	      { image: require("@/assets/img/examples/studio-4.jpg") },
+	      { image: require("@/assets/img/examples/studio-5.jpg") }
+	    ],
+	    tabPane2: [
+	      { image: require("@/assets/img/examples/olu-eletu.jpg") },
+	      { image: require("@/assets/img/examples/clem-onojeghuo.jpg") },
+	      { image: require("@/assets/img/examples/cynthia-del-rio.jpg") },
+	      { image: require("@/assets/img/examples/mariya-georgieva.jpg") },
+	      { image: require("@/assets/img/examples/clem-onojegaw.jpg") }
+	    ],
+	    tabPane3: [
+	      { image: require("@/assets/img/examples/mariya-georgieva.jpg") },
+	      { image: require("@/assets/img/examples/studio-3.jpg") },
+	      { image: require("@/assets/img/examples/clem-onojeghuo.jpg") },
+	      { image: require("@/assets/img/examples/olu-eletu.jpg") },
+	      { image: require("@/assets/img/examples/studio-1.jpg") }
+	    ],
+
+	    token: null,
+	    userInfo: {},
+	  };
+	},
+	props: {
+	  header: {
+	    type: String,
+	    default: require("@/assets/img/city-profile.jpg")
+	  },
+	  img: {
+	    type: String,
+	    default: require("@/assets/img/faces/christian.jpg")
+	  }
+	},
+	computed: {
+	  headerStyle() {
+	    return {
+	      backgroundImage: `url(${this.header})`
+	    };
+	  }
+	},
+	methods: {
+		getUserInfo() {
+			this.token = authUtils.getAuthToken();
+			if(!this.token){
+				window.history.back();
+			} else {
+				fetch(github.api.profile,
+			  		{
+			  			method: "GET",
+			  			headers: github.api.header(this.token),
+			  		}
+			  	).then(res => res.json()
+			  	).then(data => {
+					console.log(data);
+					this.userInfo = data;
+			  	}).catch(err => {
+			  		//alert(this.props.t('alert.authentication') + this.props.t('alert.error') + ": [" + err +"]");
+			  		alert("사용자 정보 조회 실패");
+				});
+			}
+		},
+	},
+
+	mounted() {
+		this.getUserInfo();
+	},
 };
 </script>
 
